7e63393bcc87bbfec39cf6cac1c37dc286088738
teresarey8 <123724097+teresarey8@users.noreply.github.com>
2023-04-13 11:12:10 +0200

    parse

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 alert("A continuación se solicitarán dos números");
-let números1 = prompt("Por favor, introduce el primer número:");
-let números2 = prompt("Por favor, introduce el segundo número:");
+let números1 = ParseFloat(prompt("Por favor, introduce el primer número:"));
+let números2 = ParseFloat(prompt("Por favor, introduce el segundo número:"));
 
 let suma = números1 + números2;
 let resta = números1 - números2;
