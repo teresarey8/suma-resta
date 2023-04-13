f49799ba80ee62618434aeee2f6748ed1a577b1f
teresarey8 <123724097+teresarey8@users.noreply.github.com>
2023-04-13 10:57:43 +0200

    2

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 alert("A continuación se solicitarán dos números");
-let números = prompt("Por favor, introduce el primer número:");
-let números = prompt("Por favor, introduce el segundo número:")
+let números1 = prompt("Por favor, introduce el primer número:");
+let números2 = prompt("Por favor, introduce el segundo número:");
