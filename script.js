c3f63c54a6b114828952b7b0aac34ad49d4f3060
teresarey8 <123724097+teresarey8@users.noreply.github.com>
2023-04-13 11:06:50 +0200

    alrtes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 alert("A continuación se solicitarán dos números");
 let números1 = prompt("Por favor, introduce el primer número:");
 let números2 = prompt("Por favor, introduce el segundo número:");
+
 let suma = números1 + números2;
 let resta = números1 - números2;
 let divión = números1 / números2;
 let multiplicación = números1*números2;
+
+alert("El resultado de la suma es: " + suma);
+alert("El resultado de la resta es: " + resta);
+alert("El resultado de la división es: " + divión);
+alert("El resultado de la multiplicación es: " + multiplicación);
