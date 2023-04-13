dcf9c2149834d57dffc7935506b0251641c4fd8e
teresarey8 <123724097+teresarey8@users.noreply.github.com>
2023-04-13 11:04:48 +0200

    s

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 alert("A continuación se solicitarán dos números");
 let números1 = prompt("Por favor, introduce el primer número:");
 let números2 = prompt("Por favor, introduce el segundo número:");
+let suma = números1 + números2;
+let resta = números1 - números2;
+let divión = números1 / números2;
+let multiplicación = números1*números2;
